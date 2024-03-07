374f24571c8304eda27b0675f83efac94e6c3259
Nhi <nhipham0902@gmail.com>
2024-03-07 10:08:35 +0100

    add next and previous page

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,35 @@ import SearchResults from "./components/SearchResults";
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [offset, setOffset] = useState(0);
 
   const loadData = async (searchQuery) => {
     if (!searchQuery) return;
     const r = await fetch(
-      `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch=${searchQuery}`
+      `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch=${searchQuery}&sroffset=${offset}`
     );
     const data = await r.json();
     setSearchResults(data.query.search);
   };
 
+  const handleNextPage = () => {
+    setOffset(offset + 10);
+  };
+
+  const handleLastPage = () => {
+    setOffset(offset - 10);
+  };
+
   useEffect(() => {
     loadData(searchQuery);
-  }, [searchQuery]);
+  }, [searchQuery, offset]);
 
   return (
     <>
       <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <SearchResults searchQuery={searchQuery} searchResults={searchResults} />
+      <button onClick={handleLastPage}>Previous</button>
+      <button onClick={handleNextPage}>Next</button>
     </>
   );
 }
